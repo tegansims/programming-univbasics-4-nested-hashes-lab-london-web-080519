24cc9170a369f181a288f86de298221b6544c2ca
Tegan Sims <tegansims@hotmail.com>
2019-06-24 16:11:47 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -56,7 +56,7 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
-p programmer_hash[:dennis_ritchie][:languages].to_s
+print programmer_hash[:dennis_ritchie][:languages].to_s
 end
 
 def adding_matz
