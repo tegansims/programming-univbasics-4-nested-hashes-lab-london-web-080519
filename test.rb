c6d70eae3e5983e1514497b28f8bbb9579add9bf
Tegan Sims <tegansims@hotmail.com>
2019-06-24 16:27:21 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,5 +1,19 @@
 new_hash = {:languages => ["C"]}
 
-new_hash[:yukihiro_matsumoto] = {}
+new_hash[:yukihiro_matsumoto] = {
+  :known_for => "Ruby",
+  :languages => ["LISP", "C"]
+}
 
 puts new_hash
+
+
+
+
+
+
+# add the following information to the top level of programmer_hash
+# :yukihiro_matsumoto => {
+#   :known_for => "Ruby",
+#    :languages => ["LISP", "C"]
+# }
