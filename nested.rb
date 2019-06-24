6b6d1adfe0f18ad9bdf9d7eb07a4e4d3c4e20f65
Tegan Sims <tegansims@hotmail.com>
2019-06-24 16:12:22 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -57,7 +57,7 @@ def dennis_ritchies_language
         }
      }
 programmer_hash[:dennis_ritchie][:languages].to_s!
-return programmer_hash[:dennis_ritchie][:languages].to_s!
+return programmer_hash[:dennis_ritchie][:languages]
 end
 
 def adding_matz
